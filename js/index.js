e1c2cbba3ccfa80f6c7a801dbce6a1410a0e36b4
Rostislav <68556673+go-rostislav@users.noreply.github.com>
2022-01-11 14:51:21 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+   $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+   items: 4,
+   loop: false,
+   center: true,
+   margin: 10,
+   URLhashListener: true,
+   autoplayHoverPause: true,
+   startPosition: 'URLHash'
+});
